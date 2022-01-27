db5179eab9a3b128b353cb87337cfc9d6162972b
Shivani Kumar <shivanikumar1920@yahoo.com>
2022-01-27 15:15:34 -0500

    Adding signup link

--- web/src/App.js
+++ web/src/App.js
@@ -1,35 +1,21 @@
-import React, { useState } from 'react';
-import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
+import React from 'react';
 import logo from './logo.svg';
 import Login from './component/Login';
-import Signup from './component/Signup';
 import useToken from './component/useToken';
 import './App.css';
 
-function App() {
+export default function App() {
   const {token, setToken} = useToken();
 
   if(!token) {
     return <Login setToken={setToken}/>
   }
+
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          <ul className="nav">
-            <Link to="/login">Login</Link>
-            <br></br>
-            <Link to="/signup">Signup</Link>
-          </ul>
-          <Switch>
-            <Route path="/login"><Login/></Route>
-            <Route path="/signup"><Signup/></Route>
-          </Switch>
-        </header>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
   );
 }
-
-export default App;
